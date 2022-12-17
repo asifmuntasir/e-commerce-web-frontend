a2238c5fc1b7425c7acb0dbdfac3d29e270de8f4
asifmuntasir <shuyeb.dk@gmail.com>
2022-12-17 22:12:23 +0600

    working with styled components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
 
 const Home = () => {
     return (
-        <div>
+        <Wrapper className='test'>
             This is Home page!
-        </div>
+        </Wrapper>
     );
 }
+
+const Wrapper = styled.section`
+    background-color: ${({ theme }) => theme.colors.bg};
+    width: 100%;
+    height: 300px;
+`;
 
 export default Home;
