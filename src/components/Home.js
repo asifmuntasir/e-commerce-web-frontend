ac78a5c24e4b158e9adb889b4042c77a18d98f58
asifmuntasir <shuyeb.dk@gmail.com>
2022-12-20 19:06:24 +0600

    services section designed completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import HeroSection from './SubComponents/HeroSection';
+import Services from './SubComponents/Services';
+import Trusted from './SubComponents/Trusted';
 
 
 const Home = () => {
     return (
-        <HeroSection name={'Googly Store'} />
+        <>
+            <HeroSection name={'Googly Store'} />
+            <Services />
+            <Trusted />
+        </>
     );
 }
 
