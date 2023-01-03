ecf8bfaff706f0de7e2574ebc69799225565e97e
asifmuntasir <shuyeb.dk@gmail.com>
2023-01-03 16:55:23 +0600

    get data from api using axios

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -1,8 +1,24 @@
-import { createContext } from "react";
+import axios from "axios";
+import { createContext, useEffect } from "react";
 
 const ContextApp = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
 const AppProvider = ({ children }) => {
+
+    const getProducts = async (url) => {
+        const response = await axios.get(url);
+        // console.log(response);
+        const products = await response.data;
+        console.log(products);
+    }
+
+    useEffect(() => {
+        getProducts(API);
+    }, [])
+
+
     return (<ContextApp.Provider value={'Googly'}>
         {children}
     </ContextApp.Provider>)
