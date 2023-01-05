532213035198967603cbb4432e2bcfc45dde9649
asifmuntasir <shuyeb.dk@gmail.com>
2023-01-05 19:58:40 +0600

    state management using reducer

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { createContext, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import reducer from '../reducers/ProductReducer';
+
 
 const ContextApp = createContext();
 
@@ -7,19 +9,41 @@ const API = "https://api.pujakaitem.com/api/products";
 
 const AppProvider = ({ children }) => {
 
+    const initialState = {
+        isLoading: false,
+        isError: false,
+        products: [],
+        featureProducts: []
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
     const getProducts = async (url) => {
-        const response = await axios.get(url);
-        // console.log(response);
-        const products = await response.data;
-        console.log(products);
+        dispatch({
+            type: "SET_LOADING",
+        });
+        try {
+            const response = await axios.get(url);
+            // console.log(response);
+            const products = await response.data;
+            // console.log(products);
+            dispatch({
+                type: "SET_API_DATA",
+                payload: products
+            });
+        } catch (error) {
+            dispatch({
+                type: "API_ERROR",
+            });
+        }
     }
 
     useEffect(() => {
         getProducts(API);
-    }, [])
+    }, []);
 
 
-    return (<ContextApp.Provider value={'Googly'}>
+    return (<ContextApp.Provider value={{ ...state }}>
         {children}
     </ContextApp.Provider>)
 }
---
+++ src/reducers/ProductReducer.js
@@ -0,0 +1,6 @@
+
+const ProductReducer = (state, action) => {
+    return state;
+}
+
+export default ProductReducer;
